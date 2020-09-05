b638bfdc3730db6640901e4cd768f475c64e7d4a
toukoum <raphaelgiraud12@gmail.com>
2020-09-05 15:01:01 +0200

    test pour la nsi

---
+++ nsi_day_1.py
@@ -0,0 +1,46 @@
+from turtle import *
+
+shape('turtle')
+
+color('red')
+
+for n in range(1, 5):
+    forward(100)
+    left(90)
+
+penup()
+goto(-200, 100)
+pendown()
+
+color('blue')
+
+for n in range(1, 7):
+    forward(100)
+    left(60)
+
+penup()
+goto(-200, -200)
+pendown()
+
+color('yellow')
+
+for n in range(1, 7):
+    forward(150)
+    left(60)
+
+penup()
+goto(110, 100)
+pendown()
+
+color('green')
+
+d=100
+
+for a in range(1, 11):
+    for b in range(1, 5):
+        forward(d)
+        left(90)
+    d+=10
+
+turtle.exitonclick()
+mainloop()
