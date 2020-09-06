023c4a75918f3e72fc66c2c56ab244402e4b1b45
toukoum <raphaelgiraud12@gmail.com>
2020-09-06 13:13:07 +0200

    presque fini wallah !

--- nsi_day_1.py
+++ nsi_day_1.py
@@ -30,6 +30,7 @@ for n in range(1, 7):
     left(60)
 
 speed(10)
+
 # les 10 carrés qui s'agrandissent au fur et à mesure 
 penup()
 goto(110, 100)
@@ -62,35 +63,111 @@ color('black')
 # -----------------------------------------
 # MON PROGRAMME :
 
-print(x_objectif, y_objectif)
+# print(x_objectif, y_objectif)
 
 
 
-compteur = 0
-yes = True 
+# compteur = 0
+# yes = True 
 
-while xcor() != x_objectif:
-    if yes:
-        fd(1)
-        compteur += 1 
+# while xcor() != x_objectif:
+#     if yes:
+#         fd(1)
+#         compteur += 1 
     
-if xcor() == x_objectif:
+# if xcor() == x_objectif:
+#     lt(90)
+#     yes = False 
+
+# while ycor() != y_objectif:
+#     fd(1)
+
+# if ycor() == y_objectif:    
+#     bgcolor('green')
+    
+# print(compteur)
+
+
+
+
+# -------------------------------------------------
+# EXERCICE 4  
+
+#1 les 10 carrés qui diminue au fur et à mesure 
+clear()
+
+speed(10)
+penup()
+goto(0,0)
+pendown()
+color('cyan')
+
+d=200
+
+for a in range(1, 11):
+    for b in range(1, 5):
+        forward(d)
+        left(90)
+    d-=10
+
+2 les 10 carrés qui diminue mais avec le meme centre 
+
+speed(3)
+clear()
+goto(0,0)
+
+d=200
+
+for a in range(1, 11):
+    for b in range(1, 5):
+        forward(d)
+        left(90)
+    lt(45)
+    up()
+    fd(7)
+    down()
+    rt(45)
+    d-=10
+
+#3 quadrillage 2x2 cote de 100
+#4 ancien logo de microsoft 
+
+speed(3)
+clear()
+up()
+goto(0,0)
+down()
+color('white')
+width(3)
+
+# définissons ce qu'est un carré
+def carre():
+  for cote in range(4):
+    fd(100)
     lt(90)
-    yes = False 
 
-while ycor() != y_objectif:
-    fd(1)
+fillcolor('green')
+begin_fill()
+carre()
+end_fill()
 
-if ycor() == y_objectif:    
-    bgcolor('green')
-    
-print(compteur)
+fillcolor('blue')
+begin_fill()
+rt(180)
+carre()
+end_fill()
 
+fillcolor('yellow')
+begin_fill()
+lt(90)
+carre()
+end_fill()
 
-
-
-
-     
+fillcolor('red')
+begin_fill()
+rt(180)
+carre()
+end_fill()
 
 
 
