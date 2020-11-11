447589502386632db154804a45dfc31858919080
toukoum <raphaelgiraud12@gmail.com>
2020-11-11 19:29:31 +0100

    manque asterisque sa mère

--- nsi_str.py
+++ nsi_str.py
@@ -1,102 +1,130 @@
 
 
-# # ____________________________________________________________
-# # EXERCICE 1
-# # ____________________________________________________________
+
+# ____________________________________________________________
+# EXERCICE 1
+# ____________________________________________________________
 
 
-# def demande() -> str:
-#     """
-#         chaine affichée caractère par caratctère
-#     """
-#     x = 1
-#     while x == 1:
-#         chaine = input('Entrez une chaine de caractère : ')
-#         if not "".join(chaine.split(' ')).isalpha(): 
-#             print("Ooops ! Ce n'est pas une chaine de caractère !")
-#             x = 1
-#         else:
-#             x = 0
-#     for i in chaine:
-#         print(i)        
+def demande() -> str:
+    """
+        chaine affichée caractère par caratctère
+    """
+    x = 1
+    while x == 1:
+        chaine = input('Entrez une chaine de caractère : ')
+        if not "".join(chaine.split(' ')).isalpha():
+            print("Ooops ! Ce n'est pas une chaine de caractère !")
+            x = 1
+        else:
+            x = 0
+    for i in chaine:
+        print(i)
 
-# print(demande())
-
-
-
-# # ____________________________________________________________
-# # EXERCICE 2
-# # ____________________________________________________________
-
-# def nbr_e() -> int:
-#     """
-#         Détermine combien il y a de 'e' dans une chaine de caractère saisie par l'utilisateur
-#     """
-#     x = 1
-#     while x == 1:
-#         chaine = input('Entrez une chaine de caractère : ')
-#         if not "".join(chaine.split(' ')).isalpha():
-#             print("Ooops ! Ce n'est pas une chaine de caractère !")
-#             x = 1
-#         else:
-#             x = 0
-#     compteur = 0
-#     for i in chaine:
-#         if i == 'e' or 'é' or 'è' or 'E' or 'É':
-#             compteur += 1 
-
-#     return compteur
-
-# print(nbr_e())
-
-
-# # ____________________________________________________________
-# # EXERCICE 3
-# # ____________________________________________________________
-
-# def double_vowels() -> str:
-#     """
-#         Fonction qui double le voyelles d'une chaines de caractère saisie pas l'utilisateur
-#     """
-#     x = 1
-#     while x == 1:
-#         chaine = input('Entrez une chaine de caractère : ')
-#         if not "".join(chaine.split(' ')).isalpha():
-#             print("Ooops ! Ce n'est pas une chaine de caractère !")
-#             x = 1
-#         else:
-#             x = 0
-#     chaine_1 = chaine.replace('a', 'aa')
-#     chaine_2 = chaine_1.replace('e', 'ee')
-#     chaine_3 = chaine_2.replace('i', 'ii')
-#     chaine_4 = chaine_3.replace('o', 'oo')
-#     chaine_5 = chaine_4.replace('u', 'uu')
-#     chaine_6 = chaine_5.replace('y', 'yy')
-#     return chaine_6
-
-# print(double_vowels())
+print(demande())
 
 
 
-# # ____________________________________________________________
-# # EXERCICE 4
-# # ____________________________________________________________
+# ____________________________________________________________
+# EXERCICE 2
+# ____________________________________________________________
 
-# def reverse() -> str:
-#     """
-#         Fonction qui inverse les lettres d'un mot saisie par l'utilisateur
-#     """
-#     x = 1
-#     while x == 1:
-#         chaine = input('Entrez une chaine de caractère : ')
-#         if not "".join(chaine.split(' ')).isalpha():
-#             print("Ooops ! Ce n'est pas une chaine de caractère !")
-#             x = 1
-#         else:
-#             x = 0
-#     return chaine[::-1]
+def nbr_e() -> int:
+    """
+        Détermine combien il y a de 'e' dans une chaine de caractère saisie par l'utilisateur
+    """
+    x = 1
+    while x == 1:
+        chaine = input('Entrez une chaine de caractère : ')
+        if not "".join(chaine.split(' ')).isalpha():
+            print("Ooops ! Ce n'est pas une chaine de caractère !")
+            x = 1
+        else:
+            x = 0
+    compteur = 0
+    for i in chaine:
+        if i == 'e' or i == 'é' or i == 'è' or i == 'E' or i == 'É':
+            compteur += 1
 
-# print(reverse())
+    return compteur
+
+print(nbr_e())
+
+
+# ____________________________________________________________
+# EXERCICE 3
+# ____________________________________________________________
+
+def double_vowels() -> str:
+    """
+        Fonction qui double le voyelles d'une chaines de caractère saisie pas l'utilisateur
+        avec la boucle while et l'indexiation
+    """
+    x = 1
+    while x == 1:
+        chaine = input('Entrez une chaine de caractère : ')
+        if not "".join(chaine.split(' ')).isalpha():
+            print("Ooops ! Ce n'est pas une chaine de caractère !")
+            x = 1
+        else:
+            x = 0
+    i = 0
+    while i<len(chaine):
+        print(chaine[i], end = '')
+        if chaine[i] == 'a' or chaine[i] == 'e' or chaine[i] == 'i' or chaine[i] == 'o' or chaine[i] == 'u' or chaine[i] == 'y':
+            print(chaine[i], end = '')
+        i += 1
+    print('\n')
+
+
+
+print(double_vowels())
+
+
+def double_vowels_bis() -> str:
+    """
+        Fonction qui double le voyelles d'une chaines de caractère saisie pas l'utilisateur
+        avec la méthode .replace
+    """
+    x = 1
+    while x == 1:
+        chaine = input('Entrez une chaine de caractère : ')
+        if not "".join(chaine.split(' ')).isalpha():
+            print("Ooops ! Ce n'est pas une chaine de caractère !")
+            x = 1
+        else:
+            x = 0
+    chaine_1 = chaine.replace('a', 'aa')
+    chaine_2 = chaine_1.replace('e', 'ee')
+    chaine_3 = chaine_2.replace('i', 'ii')
+    chaine_4 = chaine_3.replace('o', 'oo')
+    chaine_5 = chaine_4.replace('u', 'uu')
+    chaine_6 = chaine_5.replace('y', 'yy')
+    return chaine_6
+
+print(double_vowels_bis())
+
+
+
+# ____________________________________________________________
+# EXERCICE 4
+# ____________________________________________________________
+
+def reverse() -> str:
+    """
+        Fonction qui inverse les lettres d'un mot saisie par l'utilisateur
+    """
+    x = 1
+    while x == 1:
+        chaine = input('Entrez une chaine de caractère : ')
+        if not "".join(chaine.split(' ')).isalpha():
+            print("Ooops ! Ce n'est pas une chaine de caractère !")
+            x = 1
+        else:
+            x = 0
+    return chaine[::-1]
+
+print(reverse())
 
 def zorglangue() -> str:
     """
@@ -114,7 +142,7 @@ def zorglangue() -> str:
     chaine_3 = chaine_2.split(' ')
     chaine_4 = ' '.join(reversed(chaine_3))
     return chaine_4
-    
+
 print(zorglangue())
 
 # ____________________________________________________________
@@ -137,7 +165,7 @@ def palindrome() -> str:
     if chaine.replace(' ', '').lower() == chaine.replace(' ', '').lower()[::-1]:
         return True
     else:
-        return False 
+        return False
 
 print(palindrome())
 
@@ -145,13 +173,13 @@ print(palindrome())
 # EXERCICE 6
 # ____________________________________________________________
 
-# Si s est la chaîne, on la découpe en tranches de longueur 1 avec un slice de la forme 
-# s[k*p:(k+1)*p] qu’on place ensuite dans une liste en compréhension dont on rassemble les éléments tout en insérant 
+# Si s est la chaîne, on la découpe en tranches de longueur 1 avec un slice de la forme
+# s[k*p:(k+1)*p] qu’on place ensuite dans une liste en compréhension dont on rassemble les éléments tout en insérant
 # le séparateur avec la méthode join. D’où le code suivant :
 
 def asterisque(sep) -> str:
     """
-        Fonction qui intercalle des asterisque entre les mot 
+        Fonction qui intercalle des asterisque entre les mot
         d'une chaine de caractère saisie par l'utilisateur
     """
     x = 1
@@ -186,7 +214,7 @@ def compte_mots_2() -> int:
         Fonction qui revoit le nombre de mot d'une phrase (bis)
     """
     chaine = input('Entrez une chaine de caractère : ')
-    charactère_précedent = ' ' 
+    charactère_précedent = ' '
     nb_mots = 0
     for char in chaine:
         nb_mots += int(charactère_précedent == ' ' and char != ' ')
@@ -194,44 +222,3 @@ def compte_mots_2() -> int:
     return nb_mots
 
 print(compte_mots_2())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-while True:
-        try:
-            x = int(input("Please enter a number: "))
-            break
-        except ValueError:
-            print("Oops!  That was no valid number.  Try again...")
-
-
-
